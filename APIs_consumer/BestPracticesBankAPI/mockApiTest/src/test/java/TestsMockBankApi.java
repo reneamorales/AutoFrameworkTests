75335cdfac5d24ff7eb706bd83806b3045045af3
renemorales <renemoralesalejandro@gmail.com>
2024-06-27 16:01:31 -0300

    "agregando pruebas de actualización de numero de cuenta y verificaicíón de depósito correcto"

--- APIs_consumer/BestPracticesBankAPI/mockApiTest/src/test/java/TestsMockBankApi.java
+++ APIs_consumer/BestPracticesBankAPI/mockApiTest/src/test/java/TestsMockBankApi.java
@@ -1,4 +1,7 @@
+import com.google.gson.JsonObject;
+import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import io.restassured.http.ContentType;
 import net.datafaker.Faker;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
@@ -8,7 +11,7 @@ import java.util.*;
 
 import static io.restassured.RestAssured.given;
 
-public class TestsMockBankAPI {
+public class TestsMockBankApi {
 
     @Test
     public void limpiarDatosEndpoint() {
@@ -42,7 +45,7 @@ public class TestsMockBankAPI {
 
        Set<String> correos = new HashSet<>();
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 1; i <= 10; i++) {
 
             String email;
 
@@ -54,14 +57,19 @@ public class TestsMockBankAPI {
             correos.add(email);
 
             Map<String,Object> user = new HashMap<>();
+            Map<String, Object> account = new HashMap<>();
+
+            account.put("accountNumber", faker.number().randomNumber());
+            account.put("money", faker.number().randomDouble(2, 0, 800000));
+            account.put("id", i);
 
             user.put("name", faker.name().fullName());
             user.put("password", faker.internet().password());
             user.put("email", email);
             user.put("id", i);
+            user.put("account", account);
 
             users.add(user);
-
         }
 
 
@@ -71,6 +79,8 @@ public class TestsMockBankAPI {
                   .when()
                   .post("https://665145ff20f4f4c4427756bc.mockapi.io/api/v1/users");
           Assert.assertEquals(response.getStatusCode(), 201, "Error al crear el usuario: " + user.get("email"));
+
+          System.out.println(response.asString());
       }
 
 
@@ -96,4 +106,76 @@ public class TestsMockBankAPI {
         Assert.assertEquals(emailSet.size(), correos.size());
 
     }
+@Test
+public void actualizandoNumeroDeCuenta() {
+    RestAssured.baseURI = "https://665145ff20f4f4c4427756bc.mockapi.io/api/v1";
+
+    Response response = given()
+            .when()
+            .get("/users/5")
+            .then()
+            .statusCode(200)
+            .extract().response();
+
+    Map<String, Object> user = response.jsonPath().getMap("");
+
+    Map<String, Object> account = (Map<String, Object>) user.get("account");
+    account.put("accountNumber", 987654321);
+
+
+    RestAssured.given()
+            .contentType(ContentType.JSON)
+            .body(user)
+            .patch("/users/5")
+            .then()
+            .statusCode(200)
+            .log().body()
+            .extract().response();
+
+
+    response = given()
+            .when()
+            .get("/users/5")
+            .then()
+            .statusCode(200)
+            .extract().response();
+
+    System.out.println("Numero de cuenta despues de modificar " + response.asString());
+}
+
+@Test
+public void verificaciónDeDeposito(){
+
+    RestAssured.baseURI = "https://665145ff20f4f4c4427756bc.mockapi.io/api/v1";
+
+    Response response = given()
+            .get("/users/6")
+            .then()
+            .statusCode(200)
+            .extract().response();
+
+
+     Map<String, Object> user = response.jsonPath().getMap("");
+     Map<String, Object> accountMoney = (Map <String, Object>)  user.get("account");
+
+    double dineroActual = response.jsonPath().getDouble("account.money");
+    double nuevoValor = dineroActual + 320.20;
+
+
+    accountMoney.put("money", nuevoValor);
+
+     RestAssured.given()
+             .contentType(ContentType.JSON)
+             .body(user)
+             .patch("users/6")
+             .then()
+             .statusCode(200)
+             .log().body()
+             .extract().response();
+
+     System.out.println(response.asString());
+
+    }
+
+
 }
